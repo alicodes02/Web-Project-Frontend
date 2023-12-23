72921a34347792b6e66d6997798f10a334aceb88
Rayyan-Attaullah <rayyanattaullah27@gmail.com>
2023-12-23 15:38:59 +0500

    Project css added

--- src/Pages/project.js
+++ src/Pages/project.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './ProjectManagement.css'; // Create your own CSS file for styling
+import './project.css'; // Create your own CSS file for styling
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
