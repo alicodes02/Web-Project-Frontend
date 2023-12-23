19cc21ed0c0833d4047017fa29852355e76066d9
Rayyan-Attaullah <rayyanattaullah27@gmail.com>
2023-12-23 15:36:36 +0500

    Updated

--- src/Pages/project_management.js
+++ src/Pages/project_management.js
@@ -2,12 +2,11 @@ import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import Base from '../Components/Base';
 
+
 const ProjectManagement = () => {
 
   const textStyle = {
-    
     backgroundColor: 'rgb(128,0,128,0.7)',
-    backdropFilter: 'blur(5px)',
     padding: '50px',
     borderRadius: '8px',
     marginTop: '30px',
