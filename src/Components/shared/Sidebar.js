7f7cd0f2051c24a135a7d399afdd180d194d9f9b
Rayyan-Attaullah <rayyanattaullah27@gmail.com>
2023-12-24 02:58:23 +0500

    Sidebar color changed to purple

--- src/Components/shared/Sidebar.js
+++ src/Components/shared/Sidebar.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import classNames from 'classnames'
 import { Link, useLocation } from 'react-router-dom'
-import { FcBullish } from 'react-icons/fc'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUsers } from '@fortawesome/free-solid-svg-icons';
 import { HiOutlineLogout } from 'react-icons/hi'
 import { DASHBOARD_SIDEBAR_LINKS, DASHBOARD_SIDEBAR_BOTTOM_LINKS } from '../../lib/constants'
 import '../shared/styles/sidebar.css'
+
+
 
 const linkClass ='flex items-center gap-2 font-light px-3 py-2 hover:bg-violet-200 hover:no-underline active:bg-violet-200 rounded-lg text-base';
 const activeLinkClass = 'bg-violet-200 text-black rounded-lg';  
 
 export default function Sidebar() {
+ 
   return (
-    <div className="bg-custom-color w-60 p-3 flex flex-col">
+    <div className="w-60 p-3 flex flex-col" style={{ backgroundColor: '#990099' }}>
       <div className="flex items-center gap-2 px-1 py-3">
-        <FcBullish fontSize={24} />
+      <FontAwesomeIcon icon={faUsers}   style={{ marginRight: '10px'  , color:'white'}}/>
         <span className="text-neutral-200 text-lg">CollaboraHub</span>
       </div>
-      <div className="py-8 flex flex-1 flex-col gap-0.5">
+      <div className="py-8 flex flex-1 flex-col gap-0.5" >
         {DASHBOARD_SIDEBAR_LINKS.map((link) => (
           <SidebarLink key={link.key} link={link} activeLinkClass={activeLinkClass}/>
         ))}
