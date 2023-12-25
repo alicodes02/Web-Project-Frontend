8784c7d89d8dc2e1653226911373c3d0ca1335b0
Muhammad Ali <se.muhammadali01@gmail.com>
2023-12-25 12:46:03 +0500

    implemented sorting

--- src/Pages/TaskManagement.js
+++ src/Pages/TaskManagement.js
@@ -1,284 +1,213 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Modal, Table } from 'react-bootstrap';
-import CustomNavbar from '../Navbar/CustomNavbar';
 import { motion } from 'framer-motion';
 import axios from 'axios';
-import { useEffect } from 'react';
+import CustomNavbar from '../Navbar/CustomNavbar';
 import Task from './Task';
 
 const TaskManagement = () => {
+  const [tasks, setTasks] = useState([]);
+  const [employees, setEmployees] = useState([]);
+  const [show, setShow] = useState(false);
+  const [formData, setFormData] = useState({
+    title: '',
+    description: '',
+    dueDate: '',
+    priority: 'high',
+    assignee: '65873b6c4833e5d57facce4a',
+  });
+  const [filterCriteria, setFilterCriteria] = useState({
+    priority: 'all',
+    dueDate: 'all',
+  });
 
-  const textStyle = {
-    backgroundColor: 'rgb(128,0,128,0.7)',
-    padding: '50px',
-    borderRadius: '8px',
-    marginRight:'30px',
-    marginLeft:'30px',
-    marginTop: '30px',
-    marginBottom: '2%',
-    color: '#FFFFFF',
-    overflow: 'scroll'
-  };
+  const [sortedTasks, setSortedTasks] = useState([]);
 
-  const backgroundStyle = {
-
-    backgroundImage: `url('/background_vector3.jpg')`,
-    backgroundSize: '100% 100%',
-    backgroundPosition: 'center',
-    margin: '0', 
-    padding: '1%', 
-    height: '100vh', 
-    display: 'flex',
-  };
-
-
- const [tasks, setTasks] = useState([
-    { id: 1, title: '', description: '', dueDate: '', priority: '', assignee: '', status: false, progress: 0 },
- ]);
-
- const [employees, setEmployees] = useState([]);
-
- useEffect( () => {
-
+  useEffect(() => {
     fetchAllTasks();
     fetchEmployees();
+  }, []);
 
- }, [], []);
-
- const fetchEmployees = async () => {
-
-  try {
-
-    const response = await axios.get('http://localhost:3001/employees');
-
-    setEmployees(response.data.employees);
-  }
-
-  catch(error) {
-
-    const failMessage = error.response.data.message;
-
-    alert(failMessage);
-
-    console.log(error);
-
-    if (error.response) {
-      console.error('Error:', error.response.data.message);
-    } else if (error.request) {
-      console.error('Error: No response received');
-    } else {
-      console.error('Error:', error.message);
+  const fetchEmployees = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/employees');
+      setEmployees(response.data.employees);
+    } catch (error) {
+      handleFetchError(error);
     }
-}
-
-
- };
-
- const [show, setShow] = useState(false);
- const [formData, setFormData] = useState({ title: '', description: '', dueDate: '', priority: 'high', assignee: '65873b6c4833e5d57facce4a' });
-
- const handleClose = () => setShow(false);
- const handleShow = () => setShow(true);
-
- const handleChange = (e) => {
-
-  const { name, value } = e.target;
-  
-  if (name === 'assignee') {
-
-    setFormData({
-      ...formData,
-      assignee: value,
-    });
-
-  } else {
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  }
- };
-
- const handleAddTask = async(e) => {
-
-  e.preventDefault();
-
-  try {
-
-    const response =await axios.post('http://localhost:3001/add-task', formData);
-
-    const message = response.data.message;
-    alert(message);
-
-    fetchAllTasks();
-    handleClose();
-  }
-
-  catch(error) {
-
-    const failMessage = error.response.data.message;
-
-    alert(failMessage);
-
-    console.log(error);
-
-    if (error.response) {
-      console.error('Error:', error.response.data.message);
-    } else if (error.request) {
-      console.error('Error: No response received');
-    } else {
-      console.error('Error:', error.message);
-    }
-}
-
- };
-
- const fetchAllTasks = async () => {
-
-  try {
-
-    const response = await axios.get('http://localhost:3001/all-tasks');
-
-    setTasks(response.data.tasks);
-  }
-
-  catch(error) {
-
-    const failMessage = error.response.data.message;
-
-    alert(failMessage);
-
-    console.log(error);
-
-    if (error.response) {
-      console.error('Error:', error.response.data.message);
-    } else if (error.request) {
-      console.error('Error: No response received');
-    } else {
-      console.error('Error:', error.message);
-    }
-}
-
- };
-
- const handleCheck = async (taskId) => {
-
-  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImZhcmhhZEBnbWFpbC5jb20iLCJpYXQiOjE3MDM0MTg0NTF9.kIBrjD2PtyBa2Ghg-FmFmHB3D7nnXeMMljRhEYHkHYA';
-
-  const headers = {
-    'Authorization': `Bearer ${token}`,
-    'Content-Type': 'application/json',
   };
 
-  try {
-    const response = await axios.patch(`http://localhost:3001/edit-status/${taskId}`, {}, { headers });
-    console.log(response);
-    fetchAllTasks();
-  } catch (error) {
-    const failMessage = error.response.data.message;
-
-    alert(failMessage);
-
-    console.log(error);
-
-    if (error.response) {
-      console.error('Error:', error.response.data.message);
-    } else if (error.request) {
-      console.error('Error: No response received');
-    } else {
-      console.error('Error:', error.message);
+  const fetchAllTasks = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/all-tasks');
+      setTasks(response.data.tasks);
+    } catch (error) {
+      handleFetchError(error);
     }
-  }
-};
-
- 
-
- const handleProgressChange = async (taskId, value) => {
-  
-  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImZhcmhhZEBnbWFpbC5jb20iLCJpYXQiOjE3MDM0MTg0NTF9.kIBrjD2PtyBa2Ghg-FmFmHB3D7nnXeMMljRhEYHkHYA';
-
-  console.log("Value",value);
-  console.log("Id", taskId);
-
-  
-  const headers = {
-  'Authorization': `Bearer ${token}`,
-  'Content-Type': 'application/json',
   };
 
-    
+  const handleFetchError = (error) => {
+    const failMessage = error.response ? error.response.data.message : 'An error occurred';
+    alert(failMessage);
+    console.error(error);
+  };
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    if (name === 'assignee') {
+      setFormData({
+        ...formData,
+        assignee: value,
+      });
+    } else {
+      setFormData({
+        ...formData,
+        [name]: value,
+      });
+    }
+  };
+
+  const handleAddTask = async (e) => {
+    e.preventDefault();
 
     try {
+      const response = await axios.post('http://localhost:3001/add-task', formData);
+      alert(response.data.message);
+      fetchAllTasks();
+      handleClose();
+    } catch (error) {
+      handleFetchError(error);
+    }
+  };
 
-      const response = await axios.patch(`http://localhost:3001/edit-progress/${taskId}`, {progress:value}, {headers});
+  const handleCheck = async (taskId) => {
+    const token = 'your_token_here'; // Replace with your actual token
+    const headers = {
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    };
+
+    try {
+      const response = await axios.patch(`http://localhost:3001/edit-status/${taskId}`, {}, { headers });
       console.log(response);
       fetchAllTasks();
+    } catch (error) {
+      handleFetchError(error);
     }
+  };
 
-    catch(error) {
+  const handleProgressChange = async (taskId, value) => {
+    const token = 'your_token_here'; // Replace with your actual token
+    const headers = {
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    };
 
-      const failMessage = error.response.data.message;
-  
-      alert(failMessage);
-  
-      console.log(error);
-  
-      if (error.response) {
-        console.error('Error:', error.response.data.message);
-      } else if (error.request) {
-        console.error('Error: No response received');
-      } else {
-        console.error('Error:', error.message);
-      }
-  }
- };
+    try {
+      const response = await axios.patch(`http://localhost:3001/edit-progress/${taskId}`, { progress: value }, { headers });
+      console.log(response);
+      fetchAllTasks();
+    } catch (error) {
+      handleFetchError(error);
+    }
+  };
 
- return (
+  useEffect(() => {
+    let filteredTasks = tasks;
+    if (filterCriteria.priority !== 'all') {
+      filteredTasks = filteredTasks.filter((task) => task.priority === filterCriteria.priority);
+    }
+    // Add other filter conditions if needed
 
-  <div>
+    // Apply sorting
+    if (filterCriteria.dueDate !== 'all') {
+      handleSort('dueDate');
+    } else {
+      setSortedTasks(filteredTasks);
+    }
+  }, [tasks, filterCriteria]);
 
-    <CustomNavbar/>
+  const handleFilterChange = (name, value) => {
+    setFilterCriteria({
+      ...filterCriteria,
+      [name]: value,
+    });
+  };
 
-    <div style={backgroundStyle}>
+  const handleSort = (criteria) => {
+    // Implement sorting logic here
+    const sorted = [...tasks];
+    sorted.sort((a, b) => {
+      // Example: Sorting by priority (high -> medium -> low)
+      return a.priority.localeCompare(b.priority);
+    });
+    setSortedTasks(sorted);
+  };
 
-     <div style={textStyle}>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Description</th>
-            <th>Due Date</th>
-            <th>Priority</th>
-            <th>Assignee</th>
-            <th>Status</th>
-            <th>Progress</th>
-            <th>Completed</th>
-            
-          </tr>
-        </thead>
-        <tbody>
-          {tasks.map((task) => (
+  return (
+    <div>
+      <CustomNavbar />
+      <div
+        style={{
+          backgroundImage: `url('/background_vector3.jpg')`,
+          backgroundSize: '100% 100%',
+          backgroundPosition: 'center',
+          margin: '0',
+          padding: '1%',
+          height: '100vh',
+          display: 'flex',
+        }}
+      >
+        <div
+          style={{
+            backgroundColor: 'rgb(128,0,128,0.7)',
+            padding: '50px',
+            borderRadius: '8px',
+            marginRight: '30px',
+            marginLeft: '30px',
+            marginTop: '30px',
+            marginBottom: '2%',
+            color: '#FFFFFF',
+            overflow: 'scroll',
+          }}
+        >
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Due Date</th>
+                <th>Priority</th>
+                <th>Assignee</th>
+                <th>Status</th>
+                <th>Progress</th>
+                <th>Completed</th>
+              </tr>
+            </thead>
+            <tbody>
+              {sortedTasks.map((task) => (
+                <Task
+                  key={task._id}
+                  taskId={task._id}
+                  title={task.title}
+                  description={task.description}
+                  dueDate={task.dueDate}
+                  priority={task.priority}
+                  assignee={task.assignee.firstName}
+                  progress={task.progress}
+                  status={task.completed}
+                  handleProgressChange={handleProgressChange}
+                  handleCheck={handleCheck}
+                />
+              ))}
+            </tbody>
+          </Table>
 
-            <Task 
-
-              key={task._id} 
-              taskId = {task._id}
-              title = {task.title}
-              description = {task.description}
-              dueDate = {task.dueDate}
-              priority = {task.priority}
-              assignee = {task.assignee.firstName}
-              progress = {task.progress}
-              status = {task.completed}
-              handleProgressChange = {handleProgressChange}
-              handleCheck = {handleCheck}  />
-
-          ))}
-        </tbody>
-
-      </Table>
-
-      <Modal show={show} onHide={handleClose}>
+          <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Add Task</Modal.Title>
         </Modal.Header>
@@ -340,29 +269,22 @@ const TaskManagement = () => {
         </Modal.Body>
       </Modal>
 
-      
-
-            
-      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
-        <motion.button
-          type="submit"
-          className="btn btn-dark mt-3"
-          style={{ width: '30%', backgroundColor: 'purple' }}
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
-          onClick={handleShow}
-        >
-          Add Task
-        </motion.button>
+          <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+            <motion.button
+              type="submit"
+              className="btn btn-dark mt-3"
+              style={{ width: '30%', backgroundColor: 'purple' }}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              onClick={handleShow}
+            >
+              Add Task
+            </motion.button>
+          </div>
+        </div>
       </div>
-
-      
-      </div>
-
     </div>
-</div>
- );
+  );
 };
 
 export default TaskManagement;
-
