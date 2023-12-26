89637ad81c9ca5b4d6819ce4363839dd6f1c8acb
Muhammad Ali <se.muhammadali01@gmail.com>
2023-12-26 17:20:06 +0500

    Added File attach feature in Task Details Component and rendered all files

--- src/Pages/TaskDetails.js
+++ src/Pages/TaskDetails.js
@@ -1,13 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import { Card, Container, Row, Col, Form,} from 'react-bootstrap';
+import { Card, Container, Row, Col, Form } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import CustomNavbar from '../Navbar/CustomNavbar';
 import axios from 'axios';
-import { useEffect } from 'react';
 
 export default function TaskDetails() {
-
   const backgroundStyle = {
     backgroundImage: `url('/background_vector2.jpg')`,
     backgroundSize: '100% 100%',
@@ -34,13 +32,14 @@ export default function TaskDetails() {
   const { state } = location;
   const { taskId, title, description, dueDate, priority, assignee, status, progress } = state;
 
-  console.log(taskId);
+  const [file, setFile] = useState(null);
+  const [fileInput, setFileInput] = useState('');
 
   const [comments, setComments] = useState([]);
+  const [fileAttachments, setFileAttachments] = useState([]);
   const [newComment, setNewComment] = useState('');
 
   const fetchComments = async () => {
-
     const token ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImlicmFoZWVtcmVobWFuMTVAZ21haWwuY29tIiwiaWF0IjoxNzAzNDg3NDIzfQ.0Z8Us_0WOTOnLhWc7ySZcJdsFLLdqdqADjuZBw0S22k';
 
     const headers = {
@@ -49,22 +48,18 @@ export default function TaskDetails() {
     };
 
     try {
-
-      const response = await axios.get(`http://localhost:3001/get-comment/${taskId}`, {}, {headers});
+      const response = await axios.get(`http://localhost:3001/get-comment/${taskId}`, { headers });
 
       const successMessage = response.data.message;
-
       setComments(response.data.comments);
-    }
+      setFileAttachments(response.data.fileAttachments);
+    } catch (error) {
+      const failMessage = error.response?.data?.message || 'An error occurred';
 
-    catch(error) {
-
-      const failMessage = error.response.data.message;
-  
       alert(failMessage);
-  
-      console.log(error);
-  
+
+      console.log(error.response);
+
       if (error.response) {
         console.error('Error:', error.response.data.message);
       } else if (error.request) {
@@ -72,43 +67,47 @@ export default function TaskDetails() {
       } else {
         console.error('Error:', error.message);
       }
-  }
-
+    }
   };
 
-  useEffect( ()=> {
-
+  useEffect(() => {
     fetchComments();
-    console.log('Comments',comments);
+  }, []);
 
-  }, [] );
+  useEffect(() => {
+    console.log('Comments:', comments);
+    console.log('File Attachments:', fileAttachments);
+  }, [comments, fileAttachments]);
 
   const handleAddComment = async () => {
-
+    const formData = new FormData();
+    formData.append('comment', newComment);
+    formData.append('userId', '65872eee5de4c786c9627705');
+    formData.append('file', file);
     const token ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImlicmFoZWVtcmVobWFuMTVAZ21haWwuY29tIiwiaWF0IjoxNzAzNDg3NDIzfQ.0Z8Us_0WOTOnLhWc7ySZcJdsFLLdqdqADjuZBw0S22k';
 
-    const headers = {
-      'Authorization': `Bearer ${token}`,
-      'Content-Type': 'application/json',
-    };
-
-    console.log('Comment in handleComment = ', newComment);
-
     try {
+      const response = await axios.post(
+        `http://localhost:3001/add-comment/${taskId}`,
+        formData,
+        {
+          headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'multipart/form-data',
+          },
+        }
+      );
 
-      const response = await axios.post(`http://localhost:3001/add-comment/${taskId}`, { comment: newComment, userId: "65872eee5de4c786c9627705"}, {headers});
-      
+      alert(response.data.message);
+
       fetchComments();
-    }
-
-    catch(error) {
-
+    } catch(error) {
       const failMessage = error.response.data.message;
-  
+
       alert(failMessage);
-  
+
       console.log(error);
-  
+
       if (error.response) {
         console.error('Error:', error.response.data.message);
       } else if (error.request) {
@@ -116,8 +115,7 @@ export default function TaskDetails() {
       } else {
         console.error('Error:', error.message);
       }
-  }
-    
+    }
   };
 
   return (
@@ -125,7 +123,6 @@ export default function TaskDetails() {
       <CustomNavbar />
       <div style={backgroundStyle}>
         <Container className="mt-4">
-          {/* Task Details Card */}
           <div style={{ ...cardStyle, margin: '0 auto' }}>
             <Card bg="transparent">
               <Card.Body>
@@ -138,26 +135,44 @@ export default function TaskDetails() {
                     <p style={{ color: '#FFFFFF', marginTop: '2%' }}>Assignee: {assignee}</p>
                   </Col>
                   <Col md={6}>
-                     <p style={{ color: '#FFFFFF', fontWeight: 'bold'}}>Status:
-                     {status ? (
-                        <p className="text-success">Completed</p>) : 
+                    <p style={{ color: '#FFFFFF', fontWeight: 'bold' }}>Status:
+                      {status ? (
+                        <p className="text-success">Completed</p>) :
                         (
-                        <p className="text-danger">Pending</p>
+                          <p className="text-danger">Pending</p>
                         )}
-                     </p>
+                    </p>
                     <p style={{ color: '#FFFFFF', fontWeight: 'bold' }}>Progress: {progress}%</p>
                   </Col>
                 </Row>
               </Card.Body>
             </Card>
 
-            <h1 style={{fontWeight:'bold'}}>Comments Section</h1>
+            <h1 style={{ fontWeight: 'bold' }}>Comments Section</h1>
 
-            <div className="mt-4">
-              <h4 style={{ color: '#FFFFFF', marginBottom: '10px' }}>Comments</h4>
-              {comments.map((comment, index) => (
-                <p key={index} style={{ color: '#FFFFFF' }}>{comment.text}</p>
-              ))}
+                <div className="mt-4">
+                      <h4 style={{ color: '#FFFFFF', marginBottom: '10px' }}>Comments</h4>
+                      {comments.map((comment, index) => (
+                        <div key={index} style={{ color: '#FFFFFF', marginBottom: '20px' }}>
+                          {comment.text && <p>{comment.text}</p>}
+                        </div>
+                      ))}
+                      
+                      <h4 style={{ color: '#FFFFFF', marginBottom: '10px' }}>File Attachments</h4>
+                      {fileAttachments.map((fileAttachment, index) => (
+                        <div key={index} style={{ color: '#FFFFFF', marginBottom: '20px' }}>
+                          <p>Attached File:</p>
+                          <p>Filename: {fileAttachment.filename}</p>
+                          <a
+                            href={`data:application/octet-stream;base64,${fileAttachment.path}`}
+                            download={fileAttachment.filename}
+                            style={{ color: 'lightblue' }}
+                          >
+                            {fileAttachment.filename}
+                          </a>
+                        </div>
+                      ))}
+                      
               <Form>
                 <Form.Group controlId="commentForm">
                   <Form.Label style={{ color: '#FFFFFF' }}>Add a Comment</Form.Label>
@@ -168,13 +183,28 @@ export default function TaskDetails() {
                     onChange={(e) => setNewComment(e.target.value)}
                   />
                 </Form.Group>
-                    <div className="d-flex justify-content-center" style={{marginTop:'2%'}}>
-                        <Button variant="info" onClick={handleAddComment}  className="btn btn-dark" style={{ backgroundColor: 'purple', marginBottom: '1%' }} >Add Comment</Button>{' '}
-                    </div>
-
+                <div className="d-flex justify-content-center" style={{ marginTop: '2%' }}>
+                  <Button
+                    variant="info"
+                    onClick={handleAddComment}
+                    className="btn btn-dark"
+                    style={{ backgroundColor: 'purple', marginBottom: '1%' }}
+                  >
+                    Add Comment
+                  </Button>{' '}
+                </div>
               </Form>
+              <Form.Group controlId="fileUpload">
+                <Form.Label style={{ color: '#FFFFFF' }}>Upload File</Form.Label>
+                <Form.Control
+                  type="file"
+                  onChange={(e) => {
+                    setFile(e.target.files[0]);
+                    setFileInput(e.target.value);
+                  }}
+                />
+              </Form.Group>
             </div>
-
           </div>
         </Container>
       </div>
